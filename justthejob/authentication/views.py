a842d588e2274388316e0e98fb4abb133f1a8117
Karthik Radhakrishnan <Karthik Radhakrishnan>
2016-03-08 20:37:26 +0530

    added homepage

--- justthejob/authentication/views.py
+++ justthejob/authentication/views.py
@@ -9,20 +9,38 @@ from chatbot.views import personalinfo
 from django.shortcuts import redirect
 @csrf_exempt
 def login_user(request):
-    if(request.POST):
+    if(request.method=="POST"):
         data=request.POST
-        username=data['username']
-        password=data['password']
-        user=authenticate(username=username,password=password)
-        if(user is not None):
-            login(request,user)
-            return redirect(personalinfo)
-        else:
-            template = loader.get_template('authentication/index.html')
-            context={'error':1}
-            return HttpResponse(template.render(context,request))
+        if(data['title'])=='LOGIN':
+            username=data['username']
+            password=data['password']
+            print("GOT")
+            user=authenticate(username=username,password=password)
+            if(user is not None):
+                login(request,user)
+                return redirect(personalinfo)
+            else:
+                template = loader.get_template('authentication/homepage.html')
+                context={'error':2}
+                return HttpResponse(template.render(context,request))
+        elif(data['title'])=='REGISTER':
+            username=data['username']
+            password=data['password']
+            email=data['email']
+            try:
+                u=User.objects.create_user(username,email,password)
+            except Exception as e:
+                print(e)
+                template = loader.get_template('authentication/homepage.html')
+                context={'error':1}
+                return HttpResponse(template.render(context,request))
+            else:
+                u.save()
+                user=authenticate(username=username,password=password)
+                login(request,user)
+                return redirect(personalinfo)
     else:
-        template = loader.get_template('authentication/index.html')
+        template = loader.get_template('authentication/homepage.html')
         context={'error':0}
         return HttpResponse(template.render(context,request))
 
@@ -38,7 +56,7 @@ def register(request):
             u=User.objects.create_user(username,email,password)
         except Exception as e:
             print(e)
-            template = loader.get_template('authentication/register.html')
+            template = loader.get_template('authentication/homepage.html')
             context={'error':1}
             return HttpResponse(template.render(context,request))
         else:
